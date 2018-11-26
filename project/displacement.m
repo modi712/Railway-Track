e0859415a77efcf7f2efaf5e5b709e0e3d669e9b
janaksharda <41687598+janaksharda@users.noreply.github.com>
2018-11-26 14:01:17 +0530

    Update displacement.m

--- project/displacement.m
+++ project/displacement.m
@@ -8,16 +8,42 @@ function xcc = displacement(signal,blank,sampleTime,blankLength,signalLength)
         signal(2,i) = signal(2,i) - mean(2);
         signal(3,i) = signal(3,i) - mean(3);
     end
-    vector = mean/(mean(1)*mean(1)+mean(2)*mean(2)+mean(3)*mean(3));
+    %disp(signal);
+    vector = mean/(mean(1)*mean(1)+mean(2)*mean(2)+mean(3)*mean(3))^0.5;
     acc = transpose(vector)*signal;
-    vcc = zeros(signalLength,1);
-    for i=2:signalLength
-        vcc(i) = (acc(i-1) + acc(i))/2;
-        vcc(i) = vcc(i)*sampleTime;
+   % disp(acc);
+    temp = zeros(signalLength,1);
+    for i=1:signalLength
+        temp(i) = sampleTime;
     end
-    xcc = zeros(signalLength,1);
-    for i =2:signalLength
-        xcc(i) = (vcc(i-1) + vcc(i))/2;
-        xcc(i) = xcc(i)*sampleTime;
+    vcc = sampleTime*cumtrapz(acc);
+    disp(vcc);
+    temp = zeros(signalLength,1);
+    for i=1:signalLength
+        temp(i) = sampleTime;
     end
+    xcc = sampleTime*cumtrapz(vcc);
+%      time = signalLength;
+%    xcc = table(time',cdistance','g',{'l','kj'});
+%     vcc = zeros(signalLength,1);
+%     for i=2:signalLength
+%         vcc(i) = (acc(i-1) + acc(i))/2;
+%         vcc(i) = vcc(i)*sampleTime;
+%     end
+%     sum = 0;
+%     for i=2:signalLength-1
+%         sum=sum+vcc(i);
+%         vcc(i+1) =  vcc(i+1)+sum;
+%     end
+%     xcc = zeros(signalLength,1);
+%     for i =2:signalLength
+%         xcc(i) = (vcc(i-1) + vcc(i))/2;
+%         xcc(i) = xcc(i)*sampleTime;
+%     end
+%     sum = 0;
+%     for i=2:signalLength-1
+%         sum=sum+xcc(i);
+%         xcc(i+1) =  xcc(i+1)+sum;
+%     end
+%     disp(xcc(100));
 end
